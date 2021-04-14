43e1c8b786309d1507d15cbcf820316da3a29fd0
Alba <albatetanetwork@gmail.com>
2021-04-13 22:03:27 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData() {
+
+let userData = {
+    name: "Steve",
+    email: "steve@steve.com"
+};
+let configObj = {
+method: "POST",
+headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+},
+body: JSON.stringify(userData)
+};
+return fetch("http://localhost:3000/users", configObj)
+.then(function(response) {
+return response.json()
+})
+.then(function(object) {
+document.body.innerHTML = object["id"]
+})
+.catch(function(error){
+document.body.innerHTML = error.message 
+})
+}
